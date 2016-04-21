f4d7002aea33f3f7881ee9d118e7f7143e4ffb9f
Trent Nelson <trent@trent.me>
2016-04-20 22:35:59 -0400

    Initial mhook/disasm/hook work.

---
+++ Hook/Hook.h
@@ -0,0 +1,16 @@
+// Copyright(c) Trent Nelson <trent@trent.me>
+// All rights reserved.
+
+#pragma once
+
+#ifdef __cpplus
+extern "C" {
+#endif
+
+#include <Windows.h>
+#include "../Rtl/Rtl.h"
+
+
+#ifdef __cpplus
+} // extern "C"
+#endif
