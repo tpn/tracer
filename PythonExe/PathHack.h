a6239eafb57a35781eb9aad9962e44d65b8f869c
Trent Nelson <trent@trent.me>
2016-06-01 08:26:24 -0400

    Move hard-coded paths into separate file.

---
+++ PythonExe/PathHack.h
@@ -0,0 +1,3 @@
+static CONST CHAR PythonExePath[] = "C:\\Users\\r541964\\Anaconda2\\envs\\py2711\\python.exe";
+static CONST CHAR PythonDllPath[] = "C:\\Users\\r541964\\Anaconda2\\envs\\py2711\\python27.dll";
+static CONST CHAR PythonPrefix[] = "C:\\Users\\r541964\\Anaconda2\\envs\\py2711";
