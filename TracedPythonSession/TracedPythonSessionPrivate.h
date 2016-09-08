1d18140111afeff9062e93d5d13eae2c9eea544e
Trent Nelson <trent@trent.me>
2016-09-07 23:17:23 -0400

    Fix API definitions for TRACED_PYTHON_SESSION.

--- TracedPythonSession/TracedPythonSessionPrivate.h
+++ TracedPythonSession/TracedPythonSessionPrivate.h
@@ -6,15 +6,6 @@ extern "C" {
 
 #include "TracedPythonSession.h"
 
-INITIALIZE_TRACED_PYTHON_SESSION InitializeTracedPythonSession;
-DESTROY_TRACED_PYTHON_SESSION DestroyTracedPythonSession;
-
-LOAD_PATH_ENVIRONMENT_VARIABLE LoadPathEnvironmentVariable;
-DESTROY_PATH_ENVIRONMENT_VARIABLE DestroyPathEnvironmentVariable;
-
-SANITIZE_PATH_ENVIRONMENT_VARIABLE_FOR_PYTHON \
-    SanitizePathEnvironmentVariableForPython;
-
 
 #ifdef __cpp
 } // extern "C"
