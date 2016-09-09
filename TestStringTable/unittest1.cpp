a69135eba3127d8aa837a538ac53a24f5341d3a7
Trent Nelson <trent@trent.me>
2016-09-09 00:37:55 -0400

    Non-compiling (but nearly there) checkpoint commit of StringTable work.

---
+++ TestStringTable/unittest1.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../StringTable/StringTable.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestStringTable
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+                    //Assert::AreEqual()
+			// TODO: Your test code here
+		}
+
+	};
+}
