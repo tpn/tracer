ced5966b2d0193f41c20ccdf40796c5bebdaf19d
Trent Nelson <trent@trent.me>
2016-09-30 00:50:31 -0400

    Non-compiling checkpoint commit of progress.

--- TraceStore/TraceStores.c
+++ TraceStore/TraceStores.c
@@ -8,44 +8,67 @@ Module Name:
 
 Abstract:
 
-    This module implements functionality related to a collection of trace store
-    structures, referred to as "trace stores".
+    This module implements functionality related to a collection of trace
+    store structures, referred to as "trace stores".  Routines are provided
+    for getting the allocation size of a TRACE_STORES structure as well as
+    initializing and closing the structure.
 
 --*/
 
 #include "stdafx.h"
 
-DWORD
-GetTraceStoresAllocationSize(const USHORT NumberOfTraceStores)
-{
-    SHORT Delta;
-    USHORT ExtraSize;
-    USHORT DefaultNumberOfTraceStores = (
-        RTL_FIELD_SIZE(TRACE_STORES, Stores) /
-        sizeof(TRACE_STORE)
-    );
-
-    Delta = (NumberOfTraceStores - DefaultNumberOfTraceStores);
-
-    if (Delta <= 0) {
-        return sizeof(TRACE_STORES);
-    }
-
-    ExtraSize = sizeof(TRACE_STORE) * Delta * ElementsPerTraceStore;
-
-    return sizeof(TRACE_STORES) + ExtraSize;
-}
-
-
+_Use_decl_annotations_
 BOOL
 InitializeTraceStores(
-    _In_        PRTL            Rtl,
-    _In_        PWSTR           BaseDirectory,
-    _Inout_opt_ PTRACE_STORES   TraceStores,
-    _Inout_     PULONG          SizeOfTraceStores,
-    _In_opt_    PULONG          InitialFileSizes,
-    _In_        PTRACE_FLAGS    TraceFlags
+    PRTL Rtl,
+    PWSTR BaseDirectory,
+    PTRACE_STORES TraceStores,
+    PULONG SizeOfTraceStores,
+    PULONG InitialFileSizes,
+    PTRACE_FLAGS TraceFlags,
+    PTRACE_STORE_FIELD_RELOCS FieldRelocations
     )
+/*++
+
+Routine Description:
+
+    This routine initializes a TRACE_STORES structure.
+
+Arguments:
+
+    Rtl - Supplies a pointer to an RTL structure.
+
+    BaseDirectory - Supplies a pointer to a fully-qualified, NULL-terminated
+        wide character string representing the base directory to initialize
+        the individual trace stores in.
+
+    TraceStores - Supplies a pointer to a TRACE_STORES structure to initialize.
+        The caller is responsible for allocating a sufficiently-sized buffer,
+        and must provide the size of the buffer via the following parameter.
+
+    SizeOfTraceStores - Supplies a pointer to a ULONG that contains the size
+        of the buffer pointed to by the TraceStores parameter, in bytes.  The
+        actual size of the buffer used will be written to this variable when
+        the routine completes.
+
+    InitialFileSizes - Supplies a pointer to an array of ULONG values that
+        indicate the initial file sizes to be used for each trace store.  If
+        NULL, default sizes are used.  Not applicable for read-only sessions.
+
+    TraceFlags - Supplies a pointer to a TRACE_FLAGS structure, which provides
+        additional information about how the trace stores are to be initialized.
+
+    FieldRelocations - Supplies an optional pointer to an array of
+        TRACE_STORE_FIELD_RELOCS structures to use for the trace stores.
+
+Return Value:
+
+    TRUE on success, FALSE on failure.  The required size of the TraceStores
+    buffer can be obtained by passing in a NULL value for TraceStores.  The
+    size will be written to the SizeOfTraceStores variable and FALSE will be
+    returned.
+
+--*/
 {
     BOOL Success;
     BOOL Readonly;
@@ -136,6 +159,40 @@ InitializeTraceStores(
         return FALSE;
     }
 
+    //
+    // We've got sufficient buffer space, zero the memory and proceed with
+    // initialization.
+    //
+
+    SecureZeroMemory(TraceStores, TraceStoresAllocationSize);
+
+    //
+    // If field relocations have been provided, initialize them now.
+    //
+
+    if (ARGUMENT_PRESENT(FieldRelocations)) {
+        USHORT NumberOfFieldRelocationsElements;
+        PTRACE_STORE_FIELD_RELOCS Relocs;
+
+        Success = ValidateFieldRelocationsArray(FieldRelocations);
+
+        if (!Success) {
+            return FALSE;
+        }
+
+        //
+        // Enumerate through all of the trace store IDs and see if there's
+        // a corresponding entry in the relocation array for the corresponding
+        // trace store.
+        //
+
+        for (Index = 0; Index < MAX_TRACE_STORE_IDS; Index++) {
+            BOOL Found = FALSE;
+
+        }
+
+    }
+
     Path[DirectoryLength.LowPart] = L'\\';
     FileNameDest = &Path[DirectoryLength.LowPart+1];
     RemainingChars.QuadPart = (
@@ -144,16 +201,22 @@ InitializeTraceStores(
         2
     );
 
-    SecureZeroMemory(TraceStores, TraceStoresAllocationSize);
+    //
+    // Initialize struct size fields.
+    //
+
     TraceStores->SizeOfStruct = (USHORT)sizeof(TRACE_STORES);
     TraceStores->SizeOfAllocation = (USHORT)TraceStoresAllocationSize;
+
+    //
+    // Initialize create file and map view flags.
+    //
 
     if (Readonly) {
         CreateFileDesiredAccess = GENERIC_READ;
         CreateFileMappingProtectionFlags = PAGE_READONLY;
         MapViewOfFileDesiredAccess = FILE_MAP_READ;
-    }
-    else {
+    } else {
         CreateFileDesiredAccess = GENERIC_READ | GENERIC_WRITE;
         CreateFileMappingProtectionFlags = PAGE_READWRITE;
         MapViewOfFileDesiredAccess = FILE_MAP_READ | FILE_MAP_WRITE;
@@ -177,6 +240,10 @@ InitializeTraceStores(
         CreateFileFlagsAndAttributes |= FILE_FLAG_WRITE_THROUGH;
     }
 
+    //
+    // Ensure the base directory exists.
+    //
+
     Success = CreateDirectory(BaseDirectory, NULL);
     if (!Success) {
         LastError = GetLastError();
@@ -184,6 +251,12 @@ InitializeTraceStores(
             return FALSE;
         }
     }
+
+    //
+    // If we're not read-only and the compress flag has been provided, attempt
+    // to turn on directory compression.  This results in significant storage
+    // space reductions and negligible overhead, and is on by default.
+    //
 
     if (!Readonly && Compress) {
         HANDLE DirectoryHandle;
@@ -232,8 +305,9 @@ InitializeTraceStores(
 
         PTRACE_STORE TraceStore = &TraceStores->Stores[StoreIndex];
         PTRACE_STORE AllocationStore = TraceStore + 1;
-        PTRACE_STORE AddressStore = TraceStore + 2;
-        PTRACE_STORE InfoStore = TraceStore + 3;
+        PTRACE_STORE RelocationStore = TraceStore + 2;
+        PTRACE_STORE AddressStore = TraceStore + 3;
+        PTRACE_STORE InfoStore = TraceStore + 4;
 
         LPCWSTR FileName = TraceStoreFileNames[Index];
         DWORD InitialSize = Sizes[Index];
@@ -275,7 +349,8 @@ InitializeTraceStores(
             InfoStore,
             InitialSize,
             MappingSize,
-            &Flags
+            &Flags,
+            FieldRelocation
         );
 
         if (!Success) {
@@ -286,13 +361,35 @@ InitializeTraceStores(
     return TRUE;
 }
 
+_Use_decl_annotations_
 VOID
 CloseTraceStores(
-    _In_ PTRACE_STORES TraceStores
+    PTRACE_STORES TraceStores
     )
+/*++
+
+Routine Description:
+
+    This routine closes a TRACE_STORES structure that has been previously
+    initialized.
+
+Arguments:
+
+    TraceStores - Supplies a pointer to a TRACE_STORES structure to close.
+        If NULL, the routine immediately returns.
+
+Return Value:
+
+    None.
+
+--*/
 {
     USHORT Index;
     USHORT StoreIndex;
+
+    //
+    // Validate arguments.
+    //
 
     if (!TraceStores) {
         return;
@@ -302,6 +399,58 @@ CloseTraceStores(
         CloseTraceStore(&TraceStores->Stores[StoreIndex]);
     }
 }
+
+_Use_decl_annotations_
+ULONG
+GetTraceStoresAllocationSize(
+    USHORT NumberOfTraceStores
+    )
+/*++
+
+Routine Description:
+
+    This routine returns the number of bytes required to create a TRACE_STORES
+    structure for a given number of trace stores.
+
+Arguments:
+
+    NumberOfTraceStores - Supplies the number of top-level (e.g. not including
+        including supporting metadata stores) for which the allocation size
+        is to be calculated.
+
+Return Value:
+
+    Number of bytes required for the allocation.
+
+--*/
+{
+    SHORT Delta;
+    SHORT DeltaRelocs;
+    SHORT DeltaStores;
+    USHORT ExtraSize;
+    USHORT DefaultNumberOfRelocs = (
+        RTL_FIELD_SIZE(TRACE_STORES, Relocations) /
+        sizeof(TRACE_STORE_RELOC)
+    );
+    USHORT DefaultNumberOfTraceStores = (
+        RTL_FIELD_SIZE(TRACE_STORES, Stores) /
+        sizeof(TRACE_STORE)
+    );
+
+    DeltaRelocs = (NumberOfTraceStores - DefaultNumberOfRelocs);
+    DeltaStores = (NumberOfTraceStores - DefaultNumberOfTraceStores);
+
+    Delta = DeltaRelocs + DeltaStores;
+
+    if (Delta <= 0) {
+        return sizeof(TRACE_STORES);
+    }
+
+    ExtraSize = sizeof(TRACE_STORE) * Delta * ElementsPerTraceStore;
+
+    return sizeof(TRACE_STORES) + ExtraSize;
+}
+
 
 
 // vim:set ts=8 sw=4 sts=4 tw=80 expandtab                                     :
