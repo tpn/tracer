c19d5a6a1e0a6cfd74841825c8671a41b010f494
Trent Nelson <trent@trent.me>
2016-12-03 21:31:39 -0500

    Add .pgd files.

---
+++ x64/merge.bat
@@ -0,0 +1,11 @@
+pgomgr /merge Python.pgd
+pgomgr /merge PythonProject.pgd
+pgomgr /merge PythonTracer.pgd
+pgomgr /merge Rtl.pgd
+pgomgr /merge StringTable.pgd
+pgomgr /merge TlsTracerHeap.pgd
+pgomgr /merge tpython.pgd
+pgomgr /merge TracedPythonSession.pgd
+pgomgr /merge TracerConfig.pgd
+pgomgr /merge TracerHeap.pgd
+pgomgr /merge TraceStore.pgd
