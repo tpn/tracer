cca5192dc4aae108eb76a1e91ea5c6d88822d6be
Trent Nelson <trent@trent.me>
2016-12-08 20:33:16 -0500

    Add missing CountEvents flags reader.

--- PythonTracer/PythonTracer.c
+++ PythonTracer/PythonTracer.c
@@ -477,6 +477,7 @@ InitializePythonTraceContext(
     READ_REG_DWORD_FLAG(ProfileOnly, FALSE);
     READ_REG_DWORD_FLAG(TraceOnly, FALSE);
     READ_REG_DWORD_FLAG(TrackMaxRefCounts, FALSE);
+    READ_REG_DWORD_FLAG(CountEvents, FALSE);
     READ_REG_DWORD_FLAG(TraceEverything, FALSE);
     READ_REG_DWORD_FLAG(TraceNothing, FALSE);
     READ_REG_DWORD_FLAG(TraceEverythingWhenNoModuleFilterSet, FALSE);
