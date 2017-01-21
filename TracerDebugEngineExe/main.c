f3a08d29ddb14a87fa3053c3bbf9e848ef270cde
Trent Nelson <trent@trent.me>
2017-01-21 13:36:25 -0500

    Continue implementation of TraceDebugEngineExe.

--- TracerDebugEngineExe/main.c
+++ TracerDebugEngineExe/main.c
@@ -19,11 +19,69 @@ Abstract:
 ULONG
 Main(VOID)
 {
-    ULONG ExitCode = 1;
+    PRTL Rtl;
+    ULONG ExitCode;
+    ALLOCATOR Allocator;
+    PTRACER_CONFIG TracerConfig;
+    PUNICODE_STRING RegistryPath;
+    PDEBUG_ENGINE_SESSION Session;
+    DEBUG_ENGINE_SESSION_INIT_FLAGS InitFlags = { 0 };
 
+    //
+    // Initialize the default heap allocator.  This is a thin wrapper around
+    // the generic Win32 Heap functions.
+    //
+
+    if (!DefaultHeapInitializeAllocator(&Allocator)) {
+        goto Error;
+    }
+
+    //
+    // Initialize TracerConfig and Rtl.
+    //
+
+    RegistryPath = (PUNICODE_STRING)&TracerRegistryPath;
+
+    CHECKED_MSG(
+        CreateAndInitializeTracerConfigAndRtl(
+            &Allocator,
+            RegistryPath,
+            &TracerConfig,
+            &Rtl
+        ),
+        "CreateAndInitializeTracerConfigAndRtl()"
+    );
+
+    //
+    // Extract the target process ID and thread ID from the command line.
+    //
+
+    InitFlags.InitializeFromCommandLine = TRUE;
+    CHECKED_MSG(
+        Rtl->CreateAndInitializeDebugEngineSession(
+            Rtl,
+            &Allocator,
+            InitFlags,
+            &Session
+        ),
+        "CreateAndInitializeDebugEngineSession()"
+    );
+
+    //
+    // Start the session.
+    //
+
+    CHECKED_MSG(Session->Start(Session), "DebugSession->Start()");
+
+    ExitCode = 0;
+    goto End;
+
+Error:
+    ExitCode = 1;
+
+End:
     return ExitCode;
 }
-
 
 VOID
 WINAPI
