cf624c0a62a347a9fc24e23ffd522158309356c6
Trent Nelson <trent@trent.me>
2017-01-21 13:35:24 -0500

    Continue implementation of DebugEngine component.

--- Rtl/DebugEngine.c
+++ Rtl/DebugEngine.c
@@ -44,10 +44,132 @@ DEFINE_GUID_EX(IID_IDEBUG_CONTROL, 0xb86fb3b1, 0x80d4, 0x475b,
 //
 
 BOOL
-CreateDebugEngine(
+CreateIDebugControl(
+    PDEBUG_ENGINE_SESSION Session
+    )
+/*++
+
+Routine Description:
+
+    Creates an IDebugControl object.
+
+Arguments:
+
+    Session - Supplies a pointer to a DEBUG_ENGINE_SESSION structure.
+
+Return Value:
+
+    TRUE on success, FALSE otherwise.
+
+--*/
+{
+    BOOL Success;
+    PDEBUG_ENGINE Engine;
+    PDEBUGCLIENT Client;
+    PIDEBUGCLIENT IClient;
+    PDEBUGCONTROL Control;
+    PIDEBUGCONTROL IControl;
+
+    //
+    // Create the IDebugControl interface.
+    //
+
+    Engine = &Session->Engine;
+    Client = Engine->Client;
+    IClient = Engine->IClient;
+
+    CHECKED_HRESULT_MSG(
+        Client->QueryInterface(
+            IClient,
+            (REFIID)&IID_IDEBUG_CONTROL,
+            &IControl
+        ),
+        "Client->QueryInterface(&IID_IDEBUG_CONTROL)"
+    );
+
+    Control = (PDEBUGCONTROL)IControl->lpVtbl;
+
+    Engine->IID_Control = &IID_IDEBUG_CONTROL;
+    Engine->IControl = IControl;
+    Engine->Control = Control;
+
+    Success = TRUE;
+    goto End;
+
+Error:
+    Success = FALSE;
+
+End:
+    return Success;
+}
+
+BOOL
+InitializeDebugEngine(
+    PRTL Rtl,
+    PDEBUG_ENGINE Engine
+    )
+/*++
+
+Routine Description:
+
+    This routine initializes a DEBUG_ENGINE structure.
+
+Arguments:
+
+    Rtl - Supplies a pointer to an RTL structure.
+
+    Engine - Supplies a pointer to a DEBUG_ENGINE structure to initialize.
+
+Return Value:
+
+    TRUE on success, FALSE otherwise.
+
+--*/
+{
+    BOOL Success;
+    PDEBUGCLIENT Client;
+    PIDEBUGCLIENT IClient;
+
+    //
+    // Initialize COM.
+    //
+
+    CHECKED_HRESULT_MSG(Rtl->CoInitializeEx(NULL, COINIT_APARTMENTTHREADED),
+                        "Rtl->CoInitializeEx()");
+
+    //
+    // Create the IDebugClient interface.
+    //
+
+    CHECKED_HRESULT_MSG(Rtl->DebugCreate(&IID_IDEBUG_CLIENT, &IClient),
+                        "Rtl->DebugCreate(IID_IDEBUG_CLIENT)");
+
+    Client = (PDEBUGCLIENT)IClient->lpVtbl;
+
+    Engine->SizeOfStruct = sizeof(*Engine);
+
+    Engine->IID_Client = &IID_IDEBUG_CLIENT;
+    Engine->IClient = IClient;
+    Engine->Client = Client;
+
+    Success = TRUE;
+    goto End;
+
+Error:
+    Success = FALSE;
+
+
+End:
+    return Success;
+}
+
+_Use_decl_annotations_
+BOOL
+CreateAndInitializeDebugEngineSession(
     PRTL Rtl,
     PALLOCATOR Allocator,
-    PPDEBUG_ENGINE EnginePointer
+    DEBUG_ENGINE_SESSION_INIT_FLAGS InitFlags,
+    PPDEBUG_ENGINE_SESSION SessionPointer
     )
 /*++
 
@@ -71,119 +193,184 @@ Return Value:
 --*/
 {
     BOOL Success;
-    HRESULT Result;
-    PIDEBUGCLIENT IClient = NULL;
-    PIDEBUGCONTROL IControl = NULL;
-    PDEBUGCLIENT Client = NULL;
-    PDEBUGCONTROL Control = NULL;
-    PDEBUG_ENGINE Engine = NULL;
+    HMODULE Shell32Module = NULL;
+    PDEBUGCLIENT Client;
+    PDEBUGCONTROL Control;
+    PDEBUG_ENGINE Engine;
+    PDEBUG_ENGINE_SESSION Session = NULL;
+    PCOMMAND_LINE_TO_ARGVW CommandLineToArgvW;
 
-    //
-    // Validate arguments.
-    //
+    *SessionPointer = NULL;
 
-    if (!ARGUMENT_PRESENT(Rtl)) {
-        return FALSE;
-    }
+    CHECKED_MSG(InitFlags.InitializeFromCommandLine,
+                "CreateAndInitializeDebugEngineSession(): InitFlags "
+                "missing InitializeFromCommandLine bit set");
 
-    if (!ARGUMENT_PRESENT(Allocator)) {
-        return FALSE;
-    }
+    CHECKED_MSG(Rtl->LoadDbgEng(Rtl), "Rtl!LoadDbgEng()");
+    ALLOCATE_TYPE(Session, DEBUG_ENGINE_SESSION, Allocator);
 
-    if (!ARGUMENT_PRESENT(EnginePointer)) {
-        return FALSE;
-    }
+    CHECKED_MSG(InitializeDebugEngine(Rtl, &Session->Engine),
+                "InitializeDebugEngine()");
 
-    //
-    // Clear the caller's pointer up-front.
-    //
+    LOAD_LIBRARY_A(Shell32Module, Shell32);
 
-    *EnginePointer = NULL;
+    RESOLVE_FUNCTION(CommandLineToArgvW,
+                     Shell32Module,
+                     PCOMMAND_LINE_TO_ARGVW,
+                     CommandLineToArgvW);
 
-    //
-    // Initialize COM.
-    //
+    CHECKED_MSG(Session->CommandLineA = GetCommandLineA(), "GetCommandLineA()");
+    CHECKED_MSG(Session->CommandLineW = GetCommandLineW(), "GetCommandLineW()");
 
-    Result = Rtl->CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
-    if (FAILED(Result)) {
-        OutputDebugStringA("CoInitializeEx() failed.\n");
-        return FALSE;
-    }
+    Session->ArgvW = CommandLineToArgvW(Session->CommandLineW,
+                                        &Session->NumberOfArguments);
 
-    //
-    // Create the IDebugClient interface.
-    //
+    CHECKED_MSG(Session->ArgvW, "Shell32!CommandLineToArgvW()");
 
-    Result = Rtl->DebugCreate(&IID_IDEBUG_CLIENT, &IClient);
-    if (FAILED(Result)) {
-        OutputDebugStringA("DebugCreate(Client1) failed.\n");
-        goto Error;
-    }
-
-    Client = (PDEBUGCLIENT)IClient->lpVtbl;
-
-    Result = Rtl->DebugCreate(&IID_IDEBUG_CONTROL, &IControl);
-    if (FAILED(Result)) {
-        OutputDebugStringA("DebugCreate(Control7) failed.\n");
-        goto Error;
-    }
-
-    Control = (PDEBUGCONTROL)IControl->lpVtbl;
-
-    Engine = (PDEBUG_ENGINE)(
-        Allocator->Calloc(
-            Allocator->Context,
-            1,
-            sizeof(*Engine)
-        )
+    CHECKED_MSG(
+        Rtl->ArgvWToArgvA(
+            Session->ArgvW,
+            Session->NumberOfArguments,
+            &Session->ArgvA,
+            NULL,
+            Allocator
+        ),
+        "Rtl!ArgvWToArgA"
     );
 
-    if (!Engine) {
-        goto Error;
+    CHECKED_MSG(Session->NumberOfArguments == 2,
+                "Invalid usage.  Usage: TracerDebugEngine <pid>");
+
+    //
+    // Extract the process ID to attach to.
+    //
+
+    CHECKED_NTSTATUS_MSG(
+        Rtl->RtlCharToInteger(
+            Session->ArgvA[1],
+            10,
+            &Session->TargetProcessId
+        ),
+        "Rtl->RtlCharToInteger(ArgvA[1])"
+    );
+
+    //
+    // Try open a handle to the process with all access.
+    //
+
+    Session->TargetProcessHandle = OpenProcess(PROCESS_ALL_ACCESS,
+                                               TRUE,
+                                               Session->TargetProcessId);
+    if (!Session->TargetProcessHandle) {
+        OutputDebugStringA("DbgEng:OpenProcess(PROCESS_ALL_ACCESS) failed.");
     }
 
-    Engine->SizeOfStruct = sizeof(*Engine);
-    Engine->Rtl = Rtl;
-    Engine->Allocator = Allocator;
+    //
+    // Attach to the process with the debug client.
+    //
 
-    Engine->IID_Client = &IID_IDEBUG_CLIENT;
-    Engine->IClient = IClient;
-    Engine->Client = Client;
+    Engine = &Session->Engine;
+    Client = Engine->Client;
 
-    Engine->IID_Control = &IID_IDEBUG_CONTROL;
-    Engine->IControl = IControl;
-    Engine->Control = Control;
+    CHECKED_HRESULT_MSG(
+        Client->AttachProcess(
+            (PIDEBUGCLIENT)Client,
+            0,
+            Session->TargetProcessId,
+            DEBUG_ATTACH_NONINVASIVE | DEBUG_ATTACH_NONINVASIVE_NO_SUSPEND
+        ),
+        "IDebugClient->AttachProcess()"
+    );
+
+    //
+    // AttachProcess work, create a debug control object.
+    //
+
+    CHECKED_MSG(CreateIDebugControl(Session), "CreateIDebugControl(Session)");
+
+    Control = Engine->Control;
+
+    //
+    // Set the start pointer.
+    //
+
+    Session->Start = StartDebugEngineSession;
+
+    //
+    // Try disassemble a known address.
+    //
+
+    {
+        //ULONG Flags = DEBUG_DISASM_EFFECTIVE_ADDRESS;
+        ULONG Flags = 0;
+        ULONG64 Address = (ULONG64)0x000000001e0c5450;
+        CHAR Buffer[256];
+        ULONG BufferSize = sizeof(Buffer);
+        ULONG DisassemblySize;
+        ULONG64 EndOffset;
+
+        SecureZeroMemory(&Buffer, sizeof(Buffer));
+
+        CHECKED_HRESULT_MSG(
+            Control->Disassemble(
+                (PIDEBUGCONTROL)Control,
+                Address,
+                Flags,
+                (PSTR)&Buffer,
+                BufferSize,
+                &DisassemblySize,
+                &EndOffset
+            ),
+            "Control->Disassemble()"
+        );
+    }
 
     //
     // Update the caller's pointer.
     //
 
-    *EnginePointer = Engine;
+    *SessionPointer = Session;
 
     Success = TRUE;
     goto End;
 
 Error:
-
     Success = FALSE;
 
-    if (Client) {
-        Client->Release(IClient);
-        Client = NULL;
+    if (Session) {
+        MAYBE_CLOSE_HANDLE(Session->TargetProcessHandle);
     }
 
-    if (Control) {
-        Control->Release(IControl);
-        Control = NULL;
-    }
-
-    if (Engine) {
-        Allocator->FreePointer(Allocator->Context, &Engine);
-    }
+    MAYBE_FREE_POINTER(Session, Allocator);
 
 End:
+    MAYBE_FREE_LIBRARY(Shell32Module);
 
     return Success;
+}
+
+_Use_decl_annotations_
+BOOL
+StartDebugEngineSession(
+    PDEBUG_ENGINE_SESSION Session
+    )
+/*++
+
+Routine Description:
+
+    This routine starts a debug engine session.
+
+Arguments:
+
+    Session - Supplies a pointer to a DEBUG_ENGINE_SESSION.
+
+Return Value:
+
+    TRUE on success, FALSE otherwise.
+
+--*/
+{
+    return TRUE;
 }
 
 // vim:set ts=8 sw=4 sts=4 tw=80 expandtab                                     :
--- Rtl/DebugEngine.h
+++ Rtl/DebugEngine.h
@@ -26,6 +26,8 @@ extern "C" {
 #include <DbgEng.h>
 #pragma component(browser, on)
 
+#define DEBUG_ENGINE_API RTL_API
+
 //
 // Declare IIDs of the DbgEng COM classes we use.
 //
@@ -67,8 +69,213 @@ typedef IDEBUGCONTROL7VTBL   DEBUGCONTROL;
 typedef DEBUGCONTROL      *PDEBUGCONTROL;
 typedef DEBUGCONTROL    **PPDEBUGCONTROL;
 
+typedef struct IDebugEventCallbacks IDEBUGEVENTCALLBACKS;
+typedef IDEBUGEVENTCALLBACKS *PIDEBUGEVENTCALLBACKS;
+
+typedef struct IDebugEventCallbacksVtbl DEBUGEVENTCALLBACKSVTBL;
+typedef DEBUGEVENTCALLBACKSVTBL *PDEBUGEVENTCALLBACKSVTBL;
+
 //
-// Our DebugEngine-related structures and function pointer typedefs.
+//
+
+////////////////////////////////////////////////////////////////////////////////
+// COM Function Pointer Type Definitions
+////////////////////////////////////////////////////////////////////////////////
+
+//
+// IUnknown
+//
+
+typedef struct IUnknown IUNKNOWN;
+typedef IUNKNOWN *PIUNKNOWN;
+
+typedef
+HRESULT
+(STDAPICALLTYPE IUNKNOWN_QUERY_INTERFACE)(
+    _In_ PIUNKNOWN This,
+    _In_ REFIID InterfaceId,
+    _Out_ PPVOID Interface
+    );
+typedef IUNKNOWN_QUERY_INTERFACE *PIUNKNOWN_QUERY_INTERFACE;
+
+typedef
+HRESULT
+(STDAPICALLTYPE IUNKNOWN_ADD_REF)(
+    _In_ PIUNKNOWN This
+    );
+typedef IUNKNOWN_ADD_REF *PIUNKNOWN_ADD_REF;
+
+typedef
+HRESULT
+(STDAPICALLTYPE IUNKNOWN_RELEASE)(
+    _In_ PIUNKNOWN This
+    );
+typedef IUNKNOWN_RELEASE *PIUNKNOWN_RELEASE;
+
+//
+// IDebugEventCallbacks
+//
+
+typedef
+HRESULT
+(STDAPICALLTYPE DEBUG_EVENT_GET_INTEREST_MASK)(
+    _In_ PIDEBUGEVENTCALLBACKS This,
+    _Out_ PULONG Mask
+    );
+
+typedef
+HRESULT
+(STDAPICALLTYPE DEBUG_EVENT_BREAKPOINT_CALLBACK)(
+    _In_ PIDEBUGEVENTCALLBACKS This,
+    _In_ PDEBUG_BREAKPOINT Breakpoint
+    );
+typedef DEBUG_EVENT_BREAKPOINT_CALLBACK *PDEBUG_EVENT_BREAKPOINT_CALLBACK;
+
+typedef
+HRESULT
+(STDAPICALLTYPE DEBUG_EVENT_EXCEPTION_CALLBACK)(
+    _In_ PIDEBUGEVENTCALLBACKS This,
+    _In_ PEXCEPTION_RECORD64 Exception,
+    _In_ ULONG FirstChance
+    );
+typedef DEBUG_EVENT_EXCEPTION_CALLBACK *PDEBUG_EVENT_EXCEPTION;
+
+typedef
+HRESULT
+(STDAPICALLTYPE DEBUG_EVENT_CREATE_THREAD_CALLBACK)(
+    _In_ PIDEBUGEVENTCALLBACKS This,
+    _In_ ULONG64 Handle,
+    _In_ ULONG64 DataOffset,
+    _In_ ULONG64 StartOffset
+    );
+typedef DEBUG_EVENT_CREATE_THREAD_CALLBACK *PDEBUG_EVENT_CREATE_THREAD_CALLBACK;
+
+typedef
+HRESULT
+(STDAPICALLTYPE DEBUG_EVENT_EXIT_THREAD_CALLBACK)(
+    _In_ PIDEBUGEVENTCALLBACKS This,
+    _In_ ULONG ExitCode
+    );
+typedef DEBUG_EVENT_EXIT_THREAD_CALLBACK *PDEBUG_EVENT_EXIT_THREAD_CALLBACK;
+
+typedef
+HRESULT
+(STDAPICALLTYPE DEBUG_EVENT_CREATE_PROCESS_CALLBACK)(
+    _In_ PIDEBUGEVENTCALLBACKS This,
+    _In_ ULONG64 ImageFileHandle,
+    _In_ ULONG64 Handle,
+    _In_ ULONG64 BaseOffset,
+    _In_ ULONG ModuleSize,
+    _In_opt_ PCSTR ModuleName,
+    _In_opt_ PCSTR ImageName,
+    _In_ ULONG CheckSum,
+    _In_ ULONG TimeDateStamp,
+    _In_ ULONG64 InitialThreadHandle,
+    _In_ ULONG64 ThreadDataOffset,
+    _In_ ULONG64 StartOffset
+    );
+typedef DEBUG_EVENT_CREATE_PROCESS_CALLBACK
+      *PDEBUG_EVENT_CREATE_PROCESS_CALLBACK;
+
+typedef
+_Analysis_noreturn_
+HRESULT
+(STDAPICALLTYPE DEBUG_EVENT_EXIT_PROCESS_CALLBACK)(
+    _In_ PIDEBUGEVENTCALLBACKS This,
+    _In_ ULONG ExitCode
+    );
+typedef DEBUG_EVENT_EXIT_PROCESS_CALLBACK *PDEBUG_EVENT_EXIT_PROCESS_CALLBACK;
+
+typedef
+HRESULT
+(STDAPICALLTYPE DEBUG_EVENT_LOAD_MODULE_CALLBACK)(
+    _In_ PIDEBUGEVENTCALLBACKS This,
+    _In_ ULONG64 ImageFileHandle,
+    _In_ ULONG64 BaseOffset,
+    _In_ ULONG ModuleSize,
+    _In_opt_ PCSTR ModuleName,
+    _In_opt_ PCSTR ImageName,
+    _In_ ULONG CheckSum,
+    _In_ ULONG TimeDateStamp
+    );
+typedef DEBUG_EVENT_LOAD_MODULE_CALLBACK *PDEBUG_EVENT_LOAD_MODULE_CALLBACK;
+
+typedef
+HRESULT
+(STDAPICALLTYPE DEBUG_EVENT_UNLOAD_MODULE_CALLBACK)(
+    _In_ PIDEBUGEVENTCALLBACKS This,
+    _In_opt_ PCSTR ImageBaseName,
+    _In_ ULONG64 BaseOffset
+    );
+typedef DEBUG_EVENT_UNLOAD_MODULE_CALLBACK *PDEBUG_EVENT_UNLOAD_MODULE_CALLBACK;
+
+typedef
+HRESULT
+(STDAPICALLTYPE DEBUG_EVENT_SYSTEM_ERROR_CALLBACK)(
+    _In_ PIDEBUGEVENTCALLBACKS This,
+    _In_ ULONG Error,
+    _In_ ULONG Level
+    );
+typedef DEBUG_EVENT_SYSTEM_ERROR_CALLBACK *PDEBUG_EVENT_SYSTEM_ERROR_CALLBACK;
+
+typedef
+HRESULT
+(STDAPICALLTYPE DEBUG_EVENT_SESSION_STATUS_CALLBACK)(
+    _In_ PIDEBUGEVENTCALLBACKS This,
+    _In_ ULONG Status
+    );
+typedef DEBUG_EVENT_SESSION_STATUS_CALLBACK
+      *PDEBUG_EVENT_SESSION_STATUS_CALLBACK;
+
+typedef
+HRESULT
+(STDAPICALLTYPE DEBUG_EVENT_CHANGE_DEBUGGEE_STATE_CALLBACK)(
+    _In_ PIDEBUGEVENTCALLBACKS This,
+    _In_ ULONG Flags,
+    _In_ ULONG64 Argument
+    );
+typedef DEBUG_EVENT_CHANGE_DEBUGGEE_STATE_CALLBACK
+      *PDEBUG_EVENT_CHANGE_DEBUGGEE_STATE_CALLBACK;
+
+typedef
+HRESULT
+(STDAPICALLTYPE DEBUG_EVENT_CHANGE_ENGINE_STATE_CALLBACK)(
+    _In_ PIDEBUGEVENTCALLBACKS This,
+    _In_ ULONG Flags,
+    _In_ ULONG64 Argument
+    );
+typedef DEBUG_EVENT_CHANGE_ENGINE_STATE_CALLBACK
+      *PDEBUG_EVENT_CHANGE_ENGINE_STATE_CALLBACK;
+
+typedef
+HRESULT
+(STDAPICALLTYPE DEBUG_EVENT_CHANGE_SYMBOL_STATE_CALLBACK)(
+    _In_ PIDEBUGEVENTCALLBACKS This,
+    _In_ ULONG Flags,
+    _In_ ULONG64 Argument
+    );
+typedef DEBUG_EVENT_CHANGE_SYMBOL_STATE_CALLBACK
+      *PDEBUG_EVENT_CHANGE_SYMBOL_STATE_CALLBACK;
+
+
+//
+// IDebugOutputCallbacks
+//
+
+typedef
+HRESULT
+(STDAPICALLTYPE DEBUG_OUTPUT_CALLBACK)(
+    _In_ PDEBUG_OUTPUT_CALLBACKS This,
+    _In_ ULONG Mask,
+    _In_ PCSTR String
+    );
+
+////////////////////////////////////////////////////////////////////////////////
+// End COM Function Pointer Type Definitions
+////////////////////////////////////////////////////////////////////////////////
+
+//
+// DebugEngine-related structures.
 //
 
 typedef union _DEBUG_ENGINE_FLAGS {
@@ -93,13 +300,6 @@ typedef struct _Struct_size_bytes_(SizeOfStruct) _DEBUG_ENGINE {
     DEBUG_ENGINE_FLAGS Flags;
 
     //
-    // Internal fields.
-    //
-
-    PRTL Rtl;
-    PALLOCATOR Allocator;
-
-    //
     // IIDs, interfaces and vtables.
     //
 
@@ -113,18 +313,188 @@ typedef struct _Struct_size_bytes_(SizeOfStruct) _DEBUG_ENGINE {
 
 } DEBUG_ENGINE, *PDEBUG_ENGINE, **PPDEBUG_ENGINE;
 
+//
+// Define our DEBUG_ENGINE_CONTEXT structure.
+//
+
+typedef union _DEBUG_ENGINE_CONTEXT_FLAGS {
+    ULONG AsLong;
+    struct {
+        ULONG Unused:1;
+    };
+} DEBUG_ENGINE_CONTEXT_FLAGS; *PDEBUG_ENGINE_CONTEXT_FLAGS;
+
+typedef struct _Struct_size_bytes_(SizeOfStruct) _DEBUG_ENGINE_CONTEXT {
+
+    //
+    // Size of the structure, in bytes.
+    //
+
+    _Field_range_(==, sizeof(struct _DEBUG_ENGINE_CONTEXT)) ULONG SizeOfStruct;
+
+    //
+    // Flags.
+    //
+
+    DEBUG_ENGINE_CONTEXT_FLAGS Flags;
+
+    //
+    // Debug engine.
+    //
+
+    PDEBUG_ENGINE Engine;
+
+} DEBUG_ENGINE_CONTEXT, *PDEBUG_ENGINE_CONTEXT, **PPDEBUG_ENGINE_CONTEXT;
+
+//
+// Define DEBUG_ENGINE_SESSION.
+//
+
+typedef union _DEBUG_ENGINE_SESSION_FLAGS {
+    ULONG AsLong;
+    struct {
+        ULONG Unused:1;
+    };
+} DEBUG_ENGINE_SESSION_FLAGS; *PDEBUG_ENGINE_SESSION_FLAGS;
+
+//
+// Forward definition of the start fuction such that it can be referenced in
+// the struct.
+//
+
 typedef
 _Check_return_
 _Success_(return != 0)
 BOOL
-(CREATE_DEBUG_ENGINE)(
+(START_DEBUG_ENGINE_SESSION)(
+    _In_ struct _DEBUG_ENGINE_SESSION *Session
+    );
+typedef START_DEBUG_ENGINE_SESSION *PSTART_DEBUG_ENGINE_SESSION;
+
+typedef struct _Struct_size_bytes_(SizeOfStruct) _DEBUG_ENGINE_SESSION {
+
+    //
+    // Size of the structure, in bytes.
+    //
+
+    _Field_range_(==, sizeof(struct _DEBUG_ENGINE_SESSION)) ULONG SizeOfStruct;
+
+    //
+    // Flags.
+    //
+
+    DEBUG_ENGINE_SESSION_FLAGS Flags;
+
+    //
+    // Debug engine.
+    //
+
+    DEBUG_ENGINE Engine;
+
+    //
+    // The start routine for the session.
+    //
+
+    PSTART_DEBUG_ENGINE_SESSION Start;
+
+    //
+    // Rtl structure.
+    //
+
+    PRTL Rtl;
+
+    //
+    // TracerConfig structure.
+    //
+
+    struct _TRACER_CONFIG *TracerConfig;
+
+    //
+    // Commandline-related fields.
+    //
+
+    PSTR CommandLineA;
+    PWSTR CommandLineW;
+
+    STRING CommandLineString;
+    UNICODE_STRING CommandLineUnicodeString;
+
+    LONG NumberOfArguments;
+    PPSTR ArgvA;
+    PPWSTR ArgvW;
+
+    //
+    // Target information extracted from command line.
+    //
+
+    ULONG TargetProcessId;
+    ULONG TargetMainThreadId;
+
+    //
+    // Trace Session Directory.
+    //
+
+    struct _RTL_PATH TraceSessionDirectory;
+
+    //
+    // Run History registry path.
+    //
+
+    UNICODE_STRING RunHistoryRegistryPath;
+
+    //
+    // Handles to the target process and main thread.
+    //
+
+    HANDLE TargetProcessHandle;
+    HANDLE TargetMainThreadHandle;
+
+    HKEY RunHistoryRegistryKey;
+
+} DEBUG_ENGINE_SESSION, *PDEBUG_ENGINE_SESSION, **PPDEBUG_ENGINE_SESSION;
+
+//
+// Flags for CreateAndInitializeDebugEngineSession().
+//
+
+typedef union _DEBUG_ENGINE_SESSION_INIT_FLAGS {
+    ULONG AsLong;
+    struct {
+
+        //
+        // When set, extracts the target process ID and other relevant session
+        // parameters from the command line.
+        //
+
+        ULONG InitializeFromCommandLine:1;
+    };
+} DEBUG_ENGINE_SESSION_INIT_FLAGS;
+
+//
+// Public function typedefs.
+//
+
+typedef
+_Check_return_
+_Success_(return != 0)
+BOOL
+(CREATE_AND_INTIALIZE_DEBUG_ENGINE_SESSION)(
     _In_ PRTL Rtl,
     _In_ PALLOCATOR Allocator,
-    _In_ PPDEBUG_ENGINE DebugEnginePointer
+    _In_ DEBUG_ENGINE_SESSION_INIT_FLAGS Flags,
+    _Outptr_result_nullonfailure_ PPDEBUG_ENGINE_SESSION SessionPointer
     );
-typedef CREATE_DEBUG_ENGINE *PCREATE_DEBUG_ENGINE;
-RTL_API CREATE_DEBUG_ENGINE CreateDebugEngine;
+typedef CREATE_AND_INTIALIZE_DEBUG_ENGINE_SESSION
+      *PCREATE_AND_INTIALIZE_DEBUG_ENGINE_SESSION;
 
+//
+// Public function declarations.
+//
+
+#pragma component(browser, off)
+DEBUG_ENGINE_API CREATE_AND_INTIALIZE_DEBUG_ENGINE_SESSION
+                 CreateAndInitializeDebugEngineSession;
+#pragma component(browser, on)
 
 #ifdef __cplusplus
 } // extern "C"
