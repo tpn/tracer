8e14bbfce95260e6cb3924e28a0c9af2cbcdd6b9
Trent Nelson <trent@trent.me>
2017-01-30 17:09:29 -0500

    Implement DebugEngineUnassembleFunction().

--- DebugEngine/DebugEngineUnassembleFunction.c
+++ DebugEngine/DebugEngineUnassembleFunction.c
@@ -47,7 +47,92 @@ Return Value:
 
 --*/
 {
-    return FALSE;
+    BOOL Success;
+    BOOL AcquiredLock = FALSE;
+    PDEBUG_ENGINE Engine;
+    PDEBUG_ENGINE_SESSION Session;
+    WCHAR CommandBuffer[256];
+    UNICODE_STRING Command;
+
+    //
+    // Validate arguments.
+    //
+
+    if (!ARGUMENT_PRESENT(Output)) {
+        return FALSE;
+    }
+
+    //
+    // Initialize aliases.
+    //
+
+    Session = Output->Session;
+    Engine = Session->Engine;
+
+    //
+    // Initialize the stack-allocated command buffer.
+    //
+
+    Command.Length = 0;
+    Command.MaximumLength = sizeof(CommandBuffer);
+    Command.Buffer = CommandBuffer;
+
+    //
+    // Acquire the engine lock and set the current output.
+    //
+
+    AcquireDebugEngineLock(Engine);
+    AcquiredLock = TRUE;
+    Engine->CurrentOutput = Output;
+
+    Output->OutputFlags.AsULong = OutputFlags.AsULong;
+
+    //
+    // Create the command string.
+    //
+
+    Success = DebugEngineBuildCommand(Output,
+                                      UnassembleFunctionCommandId,
+                                      CommandOptions.AsULong,
+                                      FunctionName,
+                                      &Command);
+
+    if (!Success) {
+        goto Error;
+    }
+
+    //
+    // Execute the command.
+    //
+
+    QueryPerformanceCounter(&Output->Timestamp.CommandStart);
+
+    Success = DebugEngineExecuteCommand(Output);
+
+    QueryPerformanceCounter(&Output->Timestamp.CommandEnd);
+
+    if (Success) {
+        goto End;
+    }
+
+    //
+    // Intentional follow-on to Error.
+    //
+
+Error:
+    Success = FALSE;
+
+    //
+    // Intentional follow-on to End.
+    //
+
+End:
+
+    if (AcquiredLock) {
+        ReleaseDebugEngineLock(Engine);
+    }
+
+    return Success;
 }
 
 // vim:set ts=8 sw=4 sts=4 tw=80 expandtab                                     :
