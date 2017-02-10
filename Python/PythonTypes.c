48bf373346fbc8eaefb8a6f962f8bc0049f945be
Trent Nelson <trent@trent.me>
2017-02-09 23:54:08 -0500

    Add PythonTypes.

---
+++ Python/PythonTypes.c
@@ -0,0 +1,25 @@
+/*++
+
+Copyright (c) 2017 Trent Nelson <trent@trent.me>
+
+Module Name:
+
+    PythonTypes.c
+
+Abstract:
+
+    Define trace store variable types.  See TraceStore/TraceStoreTypes.c for
+    more information.
+
+--*/
+
+#include "stdafx.h"
+
+PYTHON_DATA CONST PYTHON_FUNCTION_TABLE PythonFunctionTable = { 0 };
+PYTHON_DATA CONST PYTHON_FUNCTION_TABLE_ENTRY PythonFunctionTableEntry = { 0 };
+PYTHON_DATA CONST PYTHON_PATH_TABLE PythonPathTable = { 0 };
+PYTHON_DATA CONST PYTHON_PATH_TABLE_ENTRY PythonPathTableEntry = { 0 };
+
+PYTHON_DATA CONST PYTHON_FUNCTION PythonFunction = { 0 };
+
+// vim:set ts=8 sw=4 sts=4 tw=80 expandtab                                     :
