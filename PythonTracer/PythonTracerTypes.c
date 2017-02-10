b28ab47e5417b4b3f1c9b6b7aa6feae1cf67a4ce
Trent Nelson <trent@trent.me>
2017-02-09 23:54:42 -0500

    Add PythonTracerTypes.

---
+++ PythonTracer/PythonTracerTypes.c
@@ -0,0 +1,22 @@
+/*++
+
+Copyright (c) 2017 Trent Nelson <trent@trent.me>
+
+Module Name:
+
+    PythonTracerTypes.c
+
+Abstract:
+
+    Define trace store variable types.  See TraceStore/TraceStoreTypes.c for
+    more information.
+
+--*/
+
+#include "stdafx.h"
+
+PYTHON_TRACER_DATA CONST PYTHON_TRACE_EVENT1 TraceEvent1 = { 0 };
+PYTHON_TRACER_DATA CONST PYTHON_TRACE_EVENT2 TraceEvent2 = { 0 };
+PYTHON_TRACER_DATA CONST PYTHON_TRACE_EVENT2 TraceEvent = { 0 };
+
+// vim:set ts=8 sw=4 sts=4 tw=80 expandtab                                     :
