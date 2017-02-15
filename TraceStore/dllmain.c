80f5fdb3c4ae5d679ddc16f013fdb78e69382815
Trent Nelson <trent@trent.me>
2017-02-15 13:22:30 -0500

    Add debug output to TraceStore's _DllMainCRTStartup() routine.

--- TraceStore/dllmain.c
+++ TraceStore/dllmain.c
@@ -31,10 +31,16 @@ _DllMainCRTStartup(
             }
             break;
         case DLL_THREAD_ATTACH:
+            OutputDebugStringA("TraceStore!_DllMainCRTStartup(): "
+                               "DLL_THREAD_ATTACH.\n");
             break;
         case DLL_THREAD_DETACH:
+            OutputDebugStringA("TraceStore!_DllMainCRTStartup(): "
+                               "DLL_THREAD_DETACH.\n");
             break;
         case DLL_PROCESS_DETACH:
+            OutputDebugStringA("TraceStore!_DllMainCRTStartup(): "
+                               "DLL_PROCESS_DETACH.\n");
             RundownGlobalTraceStores();
             DestroyGlobalTraceStoresRundown();
             break;
