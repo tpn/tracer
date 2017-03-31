cadc9e6928c7fd9f744f1f7d7236f8e74e00fc06
Trent Nelson <trent@trent.me>
2017-03-31 13:42:40 -0400

    Separate out the injection logic into separate files and continue implementation.

---
+++ Rtl/Injection.c
@@ -0,0 +1,149 @@
+/*++
+
+Copyright (c) 2017 Trent Nelson <trent@trent.me>
+
+Module Name:
+
+    Injection.c
+
+Abstract:
+
+    This module implements the Rtl component's remote thread and code injection
+    component.  Functions are provided for creating injection packets, adding
+    injection payloads to packets, adding symbol requests to packets, destroying
+    packets, creating remote threads, hijacking existing threads, and performing
+    the injecting.
+
+--*/
+
+#include "stdafx.h"
+
+_Use_decl_annotations_
+BOOL
+RtlCreateInjectionPacket(
+    PRTL Rtl,
+    PRTL_INJECTION_PACKET_FLAGS Flags,
+    PCUNICODE_STRING ModulePath,
+    PSTRING CallbackFunctionName,
+    ULONG SizeOfCodeInBytes,
+    PBYTE Code,
+    ULONG TargetProcessId,
+    ULONG OptionalTargetThreadId,
+    PRTL_INJECTION_ERROR InjectionError,
+    PRTL_INJECTION_PACKET *InjectionPacketPointer
+    )
+/*++
+
+Routine Description:
+
+Arguments:
+
+Return Value:
+
+    If the routine completes successfully, TRUE is returned.  If a failure
+    occurs, FALSE is returned and InjectionError is set with the relevant
+    error code.
+
+--*/
+{
+    return FALSE;
+}
+
+_Use_decl_annotations_
+BOOL
+RtlDestroyInjectionPacket(
+    PRTL Rtl,
+    PRTL_INJECTION_PACKET *PacketPointer
+    )
+/*++
+
+Routine Description:
+
+Arguments:
+
+Return Value:
+
+    If the routine completes successfully, TRUE is returned.  If a failure
+    occurs, FALSE is returned and InjectionError is set with the relevant
+    error code.
+
+--*/
+{
+    return FALSE;
+}
+
+_Use_decl_annotations_
+BOOL
+RtlAddInjectionPayload(
+    PRTL Rtl,
+    PRTL_INJECTION_PACKET Packet,
+    PRTL_INJECTION_PAYLOAD Payload,
+    PRTL_INJECTION_ERROR InjectionError
+    )
+/*++
+
+Routine Description:
+
+Arguments:
+
+Return Value:
+
+    If the routine completes successfully, TRUE is returned.  If a failure
+    occurs, FALSE is returned and InjectionError is set with the relevant
+    error code.
+
+--*/
+{
+    return FALSE;
+}
+
+_Use_decl_annotations_
+BOOL
+RtlAddInjectionSymbols(
+    PRTL Rtl,
+    PRTL_INJECTION_PACKET Packet,
+    PRTL_INJECTION_SYMBOLS Symbols,
+    PRTL_INJECTION_ERROR InjectionError
+    )
+/*++
+
+Routine Description:
+
+Arguments:
+
+Return Value:
+
+    If the routine completes successfully, TRUE is returned.  If a failure
+    occurs, FALSE is returned and InjectionError is set with the relevant
+    error code.
+
+--*/
+{
+    return FALSE;
+}
+
+_Use_decl_annotations_
+BOOL
+RtlInject(
+    PRTL Rtl,
+    PRTL_INJECTION_PACKET Packet,
+    PRTL_INJECTION_ERROR InjectionError
+    )
+/*++
+
+Routine Description:
+
+Arguments:
+
+Return Value:
+
+    If the routine completes successfully, TRUE is returned.  If a failure
+    occurs, FALSE is returned and InjectionError is set with the relevant
+    error code.
+
+--*/
+{
+    return FALSE;
+}
+
+// vim:set ts=8 sw=4 sts=4 tw=80 expandtab                                     :
--- Rtl/Rtl.h
+++ Rtl/Rtl.h
@@ -1625,7 +1625,7 @@ typedef WMEMSET *PWMEMSET;
 //
 
 //
-// Injection-related functionality.
+// Injection-related typedefs.
 //
 
 typedef
@@ -1645,7 +1645,6 @@ HMODULE
 typedef LOAD_LIBRARY_W *PLOAD_LIBRARY_W;
 
 typedef
-WINBASEAPI
 FARPROC
 (WINAPI GET_PROC_ADDRESS)(
     _In_ HMODULE hModule,
@@ -1654,7 +1653,6 @@ FARPROC
 typedef GET_PROC_ADDRESS *PGET_PROC_ADDRESS;
 
 typedef
-WINBASEAPI
 _Ret_maybenull_
 HANDLE
 (WINAPI OPEN_EVENT_A)(
@@ -1665,7 +1663,6 @@ HANDLE
 typedef OPEN_EVENT_A *POPEN_EVENT_A;
 
 typedef
-WINBASEAPI
 _Ret_maybenull_
 HANDLE
 (WINAPI OPEN_EVENT_W)(
@@ -1676,7 +1673,6 @@ HANDLE
 typedef OPEN_EVENT_W *POPEN_EVENT_W;
 
 typedef
-WINBASEAPI
 DWORD
 (WINAPI WAIT_FOR_SINGLE_OBJECT)(
     _In_ HANDLE hHandle,
@@ -1685,241 +1681,30 @@ DWORD
 typedef WAIT_FOR_SINGLE_OBJECT *PWAIT_FOR_SINGLE_OBJECT;
 
 typedef
-WINBASEAPI
+BOOL
+(WINAPI SET_EVENT)(
+    _In_ HANDLE hEvent
+    );
+typedef SET_EVENT *PSET_EVENT;
+
+typedef
+DWORD
+(WINAPI SIGNAL_OBJECT_AND_WAIT)(
+    _In_ HANDLE hObjectToSignal,
+    _In_ HANDLE hObjectToWaitOn,
+    _In_ DWORD  dwMilliseconds,
+    _In_ BOOL   bAlertable
+    );
+typedef SIGNAL_OBJECT_AND_WAIT *PSIGNAL_OBJECT_AND_WAIT;
+
+typedef
 BOOL
 (WINAPI CLOSE_HANDLE)(
     _In_ HANDLE hObject
     );
 typedef CLOSE_HANDLE *PCLOSE_HANDLE;
 
-typedef
-VOID
-(CALLBACK RTL_INJECTION_CALLBACK)(
-    _In_ struct _RTL_INJECTION_CONTEXT Context
-    );
-typedef RTL_INJECTION_CALLBACK *PRTL_INJECTION_CALLBACK;
-
-typedef union _RTL_INJECTION_PACKET_FLAGS {
-    struct _Struct_size_bytes_(sizeof(ULONG)) {
-        ULONG Unused:1;
-    };
-    LONG AsLong;
-    ULONG AsULong;
-} RTL_INJECTION_PACKET_FLAGS;
-typedef RTL_INJECTION_PACKET_FLAGS *PRTL_INJECTION_PACKET_FLAGS;
-C_ASSERT(sizeof(RTL_INJECTION_PACKET_FLAGS) == sizeof(ULONG));
-
-typedef union _RTL_INJECTION_MAGIC {
-    LARGE_INTEGER AsLargeInteger;
-    struct {
-        ULONG MagicNumber;
-        ULONG InjectionPacketCrc32;
-    };
-} RTL_INJECTION_MAGIC;
-
-typedef struct _RTL_INJECTION_PAYLOAD {
-    LIST_ENTRY ListEntry;
-    ULONGLONG NumberOfPages;
-    PVOID Buffer;
-
-    //
-    // The original address of the payload buffer in the process that requested
-    // the injection.  This will be set by the injection machinery prior to
-    // completing the injection request.  This is provided such that injected
-    // code can potentially relocate addresses contained within the payload
-    // buffer relative to an original address.
-    //
-
-    ULONG_PTR OriginalBufferAddress;
-
-    //
-    // (48 bytes consumed.)
-    //
-
-    //
-    // Pad out to 64 bytes.
-    //
-
-    ULONGLONG Padding[3];
-} RTL_INJECTION_PAYLOAD;
-typedef RTL_INJECTION_PAYLOAD *PRTL_INJECTION_PAYLOAD;
-C_ASSERT(sizeof(RTL_INJECTION_PAYLOAD) == 64);
-
-typedef struct _RTL_INJECTION_SYMBOLS {
-    LIST_ENTRY ListEntry;
-    ULONG NumberOfSymbols;
-    ULONG NumberOfModules;
-    PCSZ *SymbolNameArray;
-    PCUNICODE_STRING *ModuleNameArray;
-
-    ULONG NumberOfResolvedSymbols;
-    PULONG_PTR SymbolAddressesArray;
-    PRTL_BITMAP FailedSymbols;
-} RTL_INJECTION_SYMBOLS;
-typedef RTL_INJECTION_SYMBOLS *PRTL_INJECTION_SYMBOLS;
-
-//
-// Callers request a remote thread + DLL injection by way of the following
-// structure.
-//
-
-typedef struct _Struct_size_bytes_(SizeOfStruct) _RTL_INJECTION_PACKET {
-
-    //
-    // Size of the structure, in bytes.
-    //
-
-    _Field_range_(==, sizeof(struct _RTL_INJECTION_PACKET)) ULONG SizeOfStruct;
-
-    //
-    // Flags.
-    //
-
-    RTL_INJECTION_PACKET_FLAGS Flags;
-
-    //
-    // Packets are verified via the magic structure before being acted upon.
-    //
-
-    RTL_INJECTION_MAGIC Magic;
-
-    //
-    // Fully-qualified path name of the target library to load.
-    //
-
-    UNICODE_STRING Path;
-
-    //
-    // Name of the procedure to resolve via GetProcAddress(). The function
-    // should conform to the RTL_INJECTION_COMPLETE_CALLBACK signature, and
-    // will be invoked by a remote thread once injection has completed.
-    //
-
-    STRING CallbackFunctionName;
-
-    //
-    // Supplies the process ID to use as the target for injection.
-    //
-
-    ULONG TargetProcessId;
-
-    //
-    // Optionally supplies the ID of a thread within the targeted process to
-    // use for injection.  If this is 0, a new remote thread will be created.
-    //
-
-    ULONG OptionalTargetThreadId;
-
-    //
-    // Payload information.
-    //
-
-    LIST_ENTRY PayloadListHead;
-    ULONGLONG NumberOfPayloads;
-    PRTL_INJECTION_PAYLOAD Payloads;
-
-} RTL_INJECTION_PACKET;
-typedef RTL_INJECTION_PACKET *PRTL_INJECTION_PACKET;
-typedef const RTL_INJECTION_PACKET *PCRTL_INJECTION_PACKET;
-
-typedef union _RTL_INJECTION_ERROR {
-    struct _Struct_size_bytes_(sizeof(ULONG)) {
-        ULONG InvalidTargetProcessId:1;
-        ULONG InvalidOptionalTargetThreadId:1;
-        ULONG InvalidPacket:1;
-        ULONG InvalidPayload:1;
-        ULONG OpenTargetProcessHandleFailed:1;
-        ULONG CreateRemoteThreadFailed:1;
-        ULONG WriteRemoteMemoryFailed:1;
-    };
-    LONG AsLong;
-    ULONG AsULong;
-} RTL_INJECTION_ERROR;
-C_ASSERT(sizeof(RTL_INJECTION_ERROR) == sizeof(ULONG));
-typedef RTL_INJECTION_ERROR *PRTL_INJECTION_ERROR;
-
-typedef
-_Check_return_
-_Success_(return != 0)
-BOOL
-(RTL_CREATE_INJECTION_PACKET)(
-    _In_ struct _RTL *Rtl,
-    _In_opt_ PRTL_INJECTION_PACKET_FLAGS Flags,
-    _In_ PCUNICODE_STRING ModulePath,
-    _In_ PSTRING CallbackFunctionName,
-    _In_ ULONG TargetProcessId,
-    _In_opt_ ULONG OptionalTargetThreadId,
-    _Out_ PRTL_INJECTION_ERROR InjectionError
-    );
-typedef RTL_CREATE_INJECTION_PACKET *PRTL_CREATE_INJECTION_PACKET;
-
-typedef
-_Success_(return != 0)
-BOOL
-(RTL_DESTROY_INJECTION_PACKET)(
-    _In_ struct _RTL *Rtl,
-    _Inout_ PRTL_INJECTION_PACKET *Packet
-    );
-typedef RTL_DESTROY_INJECTION_PACKET *PRTL_DESTROY_INJECTION_PACKET;
-
-typedef
-_Check_return_
-_Success_(return != 0)
-BOOL
-(RTL_ADD_INJECTION_PAYLOAD)(
-    _In_ struct _RTL *Rtl,
-    _In_ PRTL_INJECTION_PACKET Packet,
-    _In_ PRTL_INJECTION_PAYLOAD Payload,
-    _Out_ PRTL_INJECTION_ERROR InjectionError
-    );
-typedef RTL_ADD_INJECTION_PAYLOAD *PRTL_ADD_INJECTION_PAYLOAD;
-
-/*
-typedef
-_Check_return_
-_Success_(return != 0)
-BOOL
-(RTL_CREATE_INJECTION_SYMBOLS)(
-    _In_count_(NumberOfSymbolNames) CONST PCSZ *SymbolNameArray,
-    _In_ ULONG NumberOfSymbolNames,
-    _In_count_(NumberOfSymbolAddresses) PULONG_PTR SymbolAddressArray,
-    _In_ ULONG NumberOfSymbolAddresses,
-    _In_ HMODULE Module,
-    _In_ PRTL_BITMAP FailedSymbols,
-    _Out_ PULONG NumberOfResolvedSymbolsPointer
-    );
-*/
-
-typedef
-_Check_return_
-_Success_(return != 0)
-BOOL
-(RTL_ADD_INJECTION_SYMBOLS)(
-    _In_ struct _RTL *Rtl,
-    _In_ PRTL_INJECTION_PACKET Packet,
-    _In_ PRTL_INJECTION_SYMBOLS Symbols,
-    _Out_ PRTL_INJECTION_ERROR InjectionError
-    );
-typedef RTL_ADD_INJECTION_SYMBOLS *PRTL_ADD_INJECTION_SYMBOLS;
-
-typedef
-_Check_return_
-_Success_(return == 0)
-BOOL
-(RTL_INJECT)(
-    _In_ struct _RTL *Rtl,
-    _In_ PCRTL_INJECTION_PACKET Packet,
-    _Out_ PRTL_INJECTION_ERROR InjectionError
-    );
-typedef RTL_INJECT *PRTL_INJECT;
-
-typedef
-VOID
-(CALLBACK RTL_INJECTION_COMPLETE_CALLBACK)(
-    _In_ PRTL_INJECTION_PACKET Packet,
-    _In_opt_ RTL_INJECTION_ERROR InjectionError
-    );
-typedef RTL_INJECTION_COMPLETE_CALLBACK *PRTL_INJECTION_COMPLETE_CALLBACK;
+#include "Injection.h"
 
 //
 // Miscellaneous NT/Rtl.
