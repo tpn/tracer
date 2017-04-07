e458ba357acf3f9b2b8e9d99bd7c34342bbf45d2
Trent Nelson <trent@trent.me>
2017-04-07 19:42:32 -0400

    Checkpoint commit of various fixes to private injection logic.  (WIP.)

--- Rtl/InjectionPrivate.c
+++ Rtl/InjectionPrivate.c
@@ -181,6 +181,7 @@ Return Value:
     // Set the relevant packet flag to indicate this is a callback test.
     //
 
+    Packet->Flags.AsULong = 0;
     Packet->Flags.IsMagicNumberTest = TRUE;
 
     //
@@ -318,6 +319,182 @@ Return Value:
     // Set the relevant packet flag to indicate this is a callback test.
     //
 
+    Packet->Flags.AsULong = 0;
+    Packet->Flags.IsCodeSizeQuery = TRUE;
+
+    //
+    // Call the routine.
+    //
+
+    EncounteredException = FALSE;
+
+    __try {
+
+        CodeSizeFromReturnValue = InjectionCompleteCallback(Packet);
+
+    } __except(
+        GetExceptionCode() == STATUS_IN_PAGE_ERROR       ||
+        GetExceptionCode() == EXCEPTION_ACCESS_VIOLATION ||
+        GetExceptionCode() == EXCEPTION_ILLEGAL_INSTRUCTION ?
+            EXCEPTION_EXECUTE_HANDLER :
+            EXCEPTION_CONTINUE_SEARCH) {
+
+        EncounteredException = TRUE;
+
+        switch (GetExceptionCode()) {
+            case STATUS_IN_PAGE_ERROR:
+                Error.StatusInPageErrorInCallbackTest = TRUE;
+                break;
+            case EXCEPTION_ACCESS_VIOLATION:
+                Error.AccessViolationInCallbackTest = TRUE;
+                break;
+            case EXCEPTION_ILLEGAL_INSTRUCTION:
+                Error.IllegalInstructionInCallbackTest = TRUE;
+                break;
+        }
+    }
+
+    if (EncounteredException) {
+        goto Error;
+    }
+
+    //
+    // Assume there's an error until verified otherwise.
+    //
+
+    Error.InternalError = TRUE;
+    Error.ExtractCodeSizeFailedDuringCallbackTest = TRUE;
+
+    //
+    // Verify the sizes and addresses.
+    //
+
+    Code = (ULONG_PTR)InjectionCompleteCallback;
+    Success = GetApproximateFunctionBoundaries(Code,
+                                               &StartAddress,
+                                               &EndAddress);
+
+    if (!Success) {
+        goto Error;
+    }
+
+    CodeSize = EndAddress - StartAddress;
+
+    if (CodeSize != CodeSizeFromReturnValue) {
+        goto Error;
+    }
+
+    if (CodeSize >= PAGE_SIZE) {
+        Error.CallbackCodeSizeGreaterThanOrEqualToPageSize = TRUE;
+        goto Error;
+    }
+
+    if (PointerToOffsetCrossesPageBoundary((PVOID)StartAddress, CodeSize)) {
+        Error.CallbackCodeCrossesPageBoundary = TRUE;
+        goto Error;
+    }
+
+    if (StartAddress != Context->StartAddress) {
+        goto Error;
+    }
+
+    if (EndAddress != Context->EndAddress) {
+        goto Error;
+    }
+
+    //
+    // Code size passes tests, clear the error flags we set earlier.
+    //
+
+    Error.InternalError = FALSE;
+    Error.ExtractCodeSizeFailedDuringCallbackTest = FALSE;
+
+    Success = TRUE;
+    goto End;
+
+Error:
+
+    Success = FALSE;
+
+    //
+    // Intentional follow-on to End.
+    //
+
+End:
+
+    //
+    // Update the error code and return.
+    //
+
+    InjectionError->ErrorCode = Error.ErrorCode;
+
+    return Success;
+}
+
+_Use_decl_annotations_
+BOOL
+RtlpInjectionVerifyContextCallback(
+    PRTL Rtl,
+    PRTL_INJECTION_CONTEXT Context,
+    PRTL_INJECTION_ERROR InjectionError
+    )
+/*++
+
+Routine Description:
+
+    Verifies our internal injection thread entry adheres to the injection
+    context callback protocol.
+
+Arguments:
+
+    Rtl - Supplies a pointer to an initialized RTL structure.
+
+    Context - Supplies a pointer to the injection context being tested.
+
+    InjectionError - Supplies a pointer to a variable that receives information
+        about the error, if one occurred (as indicated by this routine returning
+        FALSE).
+
+Return Value:
+
+    If the routine completes successfully, TRUE is returned.  If a failure
+    occurs, FALSE is returned and InjectionError is set with the relevant
+    error code.
+
+--*/
+{
+    BOOL Success;
+    BOOL EncounteredException;
+    ULONGLONG CodeSizeFromReturnValue;
+    ULONGLONG CodeSize;
+    ULONG_PTR Code;
+    ULONG_PTR StartAddress;
+    ULONG_PTR EndAddress;
+    ULONG_INTEGER CodeSizeLong;
+    PRTL_INJECTION_PACKET Packet;
+    RTL_INJECTION_ERROR Error;
+    LPTHREAD_START_ROUTINE ThreadEntry;
+
+    //
+    // Clear our local error variable.
+    //
+
+    Error.ErrorCode = 0;
+
+    //
+    // Wire up our local packet pointer, and extract the injection complete
+    // callback function pointer.
+    //
+
+    Packet = &Context->Packet;
+    Callback = Context->RtlRemoteInjectionInitThunk;
+    InjectionCompleteCallback = Context->Callback;
+
+    //
+    // Set the relevant packet flag to indicate this is a callback test.
+    //
+
+    Packet->Flags.AsULong = 0;
     Packet->Flags.IsCodeSizeQuery = TRUE;
 
     //
@@ -530,6 +707,19 @@ Return Value:
     }
 
     //
+    // Code size extraction was successful.  Verify our internal thread entry
+    // thunk.
+    //
+
+    Success = RtlpInjectionVerifyContextCallback(Rtl,
+                                                 Context,
+                                                 InjectionError);
+
+    if (!Success) {
+        goto Error;
+    }
+
+    //
     // If we were going to ensure the injected code had no RIP-relative
     // addresses, we'd do that now.
     //
@@ -624,15 +814,134 @@ Return Value:
 
 --*/
 {
-    if (RtlpIsInjectionMagicNumberTestInline(Packet, Token)) {
-        return TRUE;
-    }
+    if (Packet->Flags.IsMagicNumberTest) {
+        ULARGE_INTEGER Magic;
 
-    if (RtlpIsInjectionCodeSizeQueryInline(Packet, Token)) {
+        Magic.QuadPart = Packet->MagicNumber.QuadPart;
+        *((PULONGLONG)Token) = Magic.LowPart ^ Magic.HighPart;
+
+        return TRUE;
+
+    } else if (Packet->Flags.IsCodeSizeQuery) {
+        BOOL Success;
+        ULONG_PTR InstructionPointer;
+        ULONG_PTR StartAddress;
+        ULONG_PTR EndAddress;
+        ULONG_PTR CodeSize;
+        PRTL_INJECTION_CONTEXT Context;
+
+        Context = CONTAINING_RECORD(Packet,
+                                    RTL_INJECTION_CONTEXT,
+                                    Packet);
+
+
+        InstructionPointer = (ULONG_PTR)_ReturnAddress();
+
+        Success = GetApproximateFunctionBoundaries(InstructionPointer,
+                                                   &StartAddress,
+                                                   &EndAddress);
+
+        Context->StartAddress = StartAddress;
+        Context->EndAddress = EndAddress;
+
+        if (!Success) {
+            return FALSE;
+        }
+
+        CodeSize = EndAddress - StartAddress;
+
+        if (CodeSize >= PAGE_SIZE) {
+            return FALSE;
+        }
+
+        if (PointerToOffsetCrossesPageBoundary((PVOID)StartAddress, CodeSize)) {
+            return FALSE;
+        }
+
+        *((PULONGLONG)Token) = CodeSize;
+
+        Context->SizeOfCallbackCodeInBytes = (ULONG)CodeSize;
+
         return TRUE;
     }
 
     return FALSE;
+}
+
+_Use_decl_annotations_
+BOOL
+RtlpPreInjectionContextProtocolCallbackImpl(
+    PCRTL_INJECTION_CONTEXT Context,
+    PVOID Token
+    )
+/*++
+
+Routine Description:
+
+    This routine implements the injection context callback protocol required by
+    our internal code injection routines.  It should be called as the first step
+    of our injected thread's entry routine.  The code should return the value of
+    Token if this routine returns TRUE.
+
+    This implementation is responsible for obtaining the code size of the
+    injection context callback.
+
+Arguments:
+
+    Packet - Supplies a pointer to an injection packet.
+
+    ReturnValue - Supplies the address of a variable that will receive the
+        return value if this is a protocol callback.
+
+Return Value:
+
+    TRUE if this was a callback test, FALSE otherwise.  If TRUE, the caller
+    should return the value of the ReturnValue parameter.
+
+--*/
+{
+    BOOL Success;
+    ULONG_PTR InstructionPointer;
+    ULONG_PTR StartAddress;
+    ULONG_PTR EndAddress;
+    ULONG_PTR CodeSize;
+
+    //
+    // Verify this is a code size query.
+    //
+
+    if (!Context->Flags.IsCodeSizeQuery) {
+        return FALSE;
+    }
+
+    InstructionPointer = (ULONG_PTR)_ReturnAddress();
+
+    Success = GetApproximateFunctionBoundaries(InstructionPointer,
+                                               &StartAddress,
+                                               &EndAddress);
+
+    Context->StartAddress = StartAddress;
+    Context->EndAddress = EndAddress;
+
+    if (!Success) {
+        return FALSE;
+    }
+
+    CodeSize = EndAddress - StartAddress;
+
+    if (CodeSize >= PAGE_SIZE) {
+        return FALSE;
+    }
+
+    if (PointerToOffsetCrossesPageBoundary((PVOID)StartAddress, CodeSize)) {
+        return FALSE;
+    }
+
+    *((PULONGLONG)Token) = CodeSize;
+
+    Context->SizeOfCallbackCodeInBytes = (ULONG)CodeSize;
+
+    return TRUE;
 }
 
 _Use_decl_annotations_
