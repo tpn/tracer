d11321cd6fcf8f2f32490ab48f7b12b1fe0248e7
Trent Nelson <trent@trent.me>
2017-04-30 18:18:00 -0400

    Add Cuda.h stub.

---
+++ Rtl/Cuda.h
@@ -0,0 +1,18 @@
+/*++
+
+Copyright (c) 2017 Trent Nelson <trent@trent.me>
+
+Module Name:
+
+    Cuda.h
+
+Abstract:
+
+    WIP.
+
+--*/
+
+#include "stdafx.h"
+
+
+// vim:set ts=8 sw=4 sts=4 tw=80 expandtab                                     :
