64bfa2dab83d1a433d1d783d6eb1f6cdf56c715a
Trent Nelson <trent@trent.me>
2018-04-10 15:59:31 -0400

    Implement new allocator methods for trace store component.

--- TraceStore/TraceStoreAllocator.c
+++ TraceStore/TraceStoreAllocator.c
@@ -268,6 +268,479 @@ TraceStoreDestroyAllocator(
     return;
 }
 
+//
+// Aligned variants.
+//
+
+_Use_decl_annotations_
+BOOLEAN
+TraceStoreAllocatorWriteBytes(
+    PVOID Context,
+    const VOID *Source,
+    SIZE_T SizeInBytes
+    )
+{
+    PVOID Dest;
+
+    __debugbreak();
+
+    return FALSE;
+
+    Dest = TraceStoreAllocatorMalloc(Context, SizeInBytes);
+    if (!Dest) {
+        return FALSE;
+    }
+
+    CopyMemory(Dest, Source, SizeInBytes);
+
+    return TRUE;
+}
+
+//
+// Define helper function for determining the alignment of the next address for
+// a trace store's active memory map.
+//
+
+BOOL
+TraceStoreCheckAlignment(
+    PTRACE_STORE TraceStore,
+    ULONGLONG Alignment,
+    ULONGLONG Offset
+    )
+{
+    PVOID NextAddress;
+    ULONGLONG NextAddressAlignment;
+    volatile TRACE_STORE_MEMORY_MAP *MemoryMap;
+
+    MemoryMap = (volatile TRACE_STORE_MEMORY_MAP *)TraceStore->MemoryMap;
+
+    //
+    // The MemoryMap may be NULL if this is the first allocation and
+    // the trace store hasn't completed initialization yet.  Just return
+    // TRUE for now; it'll bomb out later if the alignment request can't
+    // be satisfied (highly unlikely for the first allocation as it'll be
+    // page-size aligned at the very least).
+    //
+
+    if (!MemoryMap || !MemoryMap->NextAddress) {
+        return TRUE;
+    }
+
+    NextAddress = RtlOffsetToPointer(MemoryMap->NextAddress, Offset);
+    NextAddressAlignment = GetAddressAlignment(NextAddress);
+
+    return (Alignment < NextAddressAlignment);
+}
+
+//
+// Define aligned offset malloc-oriented functions.
+//
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorAlignedOffsetMallocWithTimestamp(
+    PVOID Context,
+    SIZE_T Size,
+    SIZE_T Alignment,
+    SIZE_T Offset,
+    PLARGE_INTEGER TimestampPointer
+    )
+{
+    PVOID Address;
+    PTRACE_STORE TraceStore;
+    PTRACE_CONTEXT TraceContext;
+    ULONG_PTR NumberOfRecords = 1;
+
+    TraceStore = ContextToTraceStore(Context);
+    TraceContext = TraceStore->TraceContext;
+
+    if (!TraceStoreCheckAlignment(TraceStore, Alignment, Offset)) {
+        return NULL;
+    }
+
+    Address = TraceStore->AllocateRecordsWithTimestamp(TraceContext,
+                                                       TraceStore,
+                                                       NumberOfRecords,
+                                                       Size,
+                                                       TimestampPointer);
+
+    return Address;
+}
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorTryAlignedOffsetMallocWithTimestamp(
+    PVOID Context,
+    SIZE_T Size,
+    SIZE_T Alignment,
+    SIZE_T Offset,
+    PLARGE_INTEGER TimestampPointer
+    )
+{
+    PVOID Address;
+    PTRACE_STORE TraceStore;
+    PTRACE_CONTEXT TraceContext;
+    ULONG_PTR NumberOfRecords = 1;
+
+    TraceStore = ContextToTraceStore(Context);
+    TraceContext = TraceStore->TraceContext;
+
+    if (!TraceStoreCheckAlignment(TraceStore, Alignment, Offset)) {
+        return NULL;
+    }
+
+    Address = TraceStore->TryAllocateRecordsWithTimestamp(TraceContext,
+                                                          TraceStore,
+                                                          NumberOfRecords,
+                                                          Size,
+                                                          TimestampPointer);
+
+    return Address;
+}
+
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorAlignedOffsetMalloc(
+    PVOID Context,
+    SIZE_T Size,
+    SIZE_T Alignment,
+    SIZE_T Offset
+    )
+{
+    return TraceStoreAllocatorAlignedOffsetMallocWithTimestamp(
+        Context,
+        Size,
+        Alignment,
+        Offset,
+        NULL
+    );
+}
+
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorTryAlignedOffsetMalloc(
+    PVOID Context,
+    SIZE_T Size,
+    SIZE_T Alignment,
+    SIZE_T Offset
+    )
+{
+    return TraceStoreAllocatorTryAlignedOffsetMallocWithTimestamp(
+        Context,
+        Size,
+        Alignment,
+        Offset,
+        NULL
+    );
+}
+
+
+//
+// Define aligned malloc-oriented functions.
+//
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorAlignedMalloc(
+    PVOID Context,
+    SIZE_T Size,
+    SIZE_T Alignment
+    )
+{
+    return TraceStoreAllocatorAlignedOffsetMallocWithTimestamp(
+        Context,
+        Size,
+        Alignment,
+        0,
+        NULL
+    );
+}
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorAlignedMallocWithTimestamp(
+    PVOID Context,
+    SIZE_T Size,
+    SIZE_T Alignment,
+    PLARGE_INTEGER TimestampPointer
+    )
+{
+    return TraceStoreAllocatorAlignedOffsetMallocWithTimestamp(
+        Context,
+        Size,
+        Alignment,
+        0,
+        TimestampPointer
+    );
+}
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorTryAlignedMalloc(
+    PVOID Context,
+    SIZE_T Size,
+    SIZE_T Alignment
+    )
+{
+    return TraceStoreAllocatorTryAlignedOffsetMallocWithTimestamp(
+        Context,
+        Size,
+        Alignment,
+        0,
+        NULL
+    );
+}
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorTryAlignedMallocWithTimestamp(
+    PVOID Context,
+    SIZE_T Size,
+    SIZE_T Alignment,
+    PLARGE_INTEGER TimestampPointer
+    )
+{
+    return TraceStoreAllocatorTryAlignedOffsetMallocWithTimestamp(
+        Context,
+        Size,
+        Alignment,
+        0,
+        TimestampPointer
+    );
+}
+
+//
+// Define aligned offset calloc-oriented functions.
+//
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorAlignedOffsetCallocWithTimestamp(
+    PVOID Context,
+    SIZE_T NumberOfElements,
+    SIZE_T ElementSize,
+    SIZE_T Alignment,
+    SIZE_T Offset,
+    PLARGE_INTEGER TimestampPointer
+    )
+{
+    PVOID Address;
+    PTRACE_STORE TraceStore;
+    PTRACE_CONTEXT TraceContext;
+
+    TraceStore = ContextToTraceStore(Context);
+    TraceContext = TraceStore->TraceContext;
+
+    if (!TraceStoreCheckAlignment(TraceStore, Alignment, Offset)) {
+        return NULL;
+    }
+
+    Address = TraceStore->AllocateRecordsWithTimestamp(TraceContext,
+                                                       TraceStore,
+                                                       NumberOfElements,
+                                                       ElementSize,
+                                                       TimestampPointer);
+
+    return Address;
+}
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorTryAlignedOffsetCallocWithTimestamp(
+    PVOID Context,
+    SIZE_T NumberOfElements,
+    SIZE_T ElementSize,
+    SIZE_T Alignment,
+    SIZE_T Offset,
+    PLARGE_INTEGER TimestampPointer
+    )
+{
+    PVOID Address;
+    PTRACE_STORE TraceStore;
+    PTRACE_CONTEXT TraceContext;
+
+    TraceStore = ContextToTraceStore(Context);
+    TraceContext = TraceStore->TraceContext;
+
+    if (!TraceStoreCheckAlignment(TraceStore, Alignment, Offset)) {
+        return NULL;
+    }
+
+    Address = TraceStore->TryAllocateRecordsWithTimestamp(TraceContext,
+                                                          TraceStore,
+                                                          NumberOfElements,
+                                                          ElementSize,
+                                                          TimestampPointer);
+
+    return Address;
+}
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorAlignedOffsetCalloc(
+    PVOID Context,
+    SIZE_T NumberOfElements,
+    SIZE_T ElementSize,
+    SIZE_T Alignment,
+    SIZE_T Offset
+    )
+{
+    return TraceStoreAllocatorAlignedOffsetCallocWithTimestamp(
+        Context,
+        NumberOfElements,
+        ElementSize,
+        Alignment,
+        Offset,
+        NULL
+    );
+}
+
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorTryAlignedOffsetCalloc(
+    PVOID Context,
+    SIZE_T NumberOfElements,
+    SIZE_T ElementSize,
+    SIZE_T Alignment,
+    SIZE_T Offset
+    )
+{
+    return TraceStoreAllocatorTryAlignedOffsetCallocWithTimestamp(
+        Context,
+        NumberOfElements,
+        ElementSize,
+        Alignment,
+        Offset,
+        NULL
+    );
+}
+
+//
+// Define aligned calloc-oriented functions.
+//
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorAlignedCallocWithTimestamp(
+    PVOID Context,
+    SIZE_T NumberOfElements,
+    SIZE_T ElementSize,
+    SIZE_T Alignment,
+    PLARGE_INTEGER TimestampPointer
+    )
+{
+    return TraceStoreAllocatorAlignedOffsetCallocWithTimestamp(
+        Context,
+        NumberOfElements,
+        ElementSize,
+        Alignment,
+        0,
+        TimestampPointer
+    );
+}
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorTryAlignedCalloc(
+    PVOID Context,
+    SIZE_T NumberOfElements,
+    SIZE_T ElementSize,
+    SIZE_T Alignment
+    )
+{
+    return TraceStoreAllocatorTryAlignedOffsetCallocWithTimestamp(
+        Context,
+        NumberOfElements,
+        ElementSize,
+        Alignment,
+        0,
+        NULL
+    );
+}
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorAlignedCalloc(
+    PVOID Context,
+    SIZE_T NumberOfElements,
+    SIZE_T ElementSize,
+    SIZE_T Alignment
+    )
+{
+    return TraceStoreAllocatorAlignedOffsetCallocWithTimestamp(
+        Context,
+        NumberOfElements,
+        ElementSize,
+        Alignment,
+        0,
+        NULL
+    );
+}
+
+_Use_decl_annotations_
+PVOID
+TraceStoreAllocatorTryAlignedCallocWithTimestamp(
+    PVOID Context,
+    SIZE_T NumberOfElements,
+    SIZE_T ElementSize,
+    SIZE_T Alignment,
+    PLARGE_INTEGER TimestampPointer
+    )
+{
+    return TraceStoreAllocatorTryAlignedOffsetCallocWithTimestamp(
+        Context,
+        NumberOfElements,
+        ElementSize,
+        Alignment,
+        0,
+        TimestampPointer
+    );
+}
+
+//
+// Define aligned free functions.
+//
+
+_Use_decl_annotations_
+VOID
+TraceStoreAllocatorAlignedFree(
+    PVOID Context,
+    PVOID Buffer
+    )
+{
+    return;
+}
+
+_Use_decl_annotations_
+VOID
+TraceStoreAllocatorAlignedFreePointer(
+    PVOID Context,
+    PPVOID BufferPointer
+    )
+{
+    if (!ARGUMENT_PRESENT(BufferPointer)) {
+        return;
+    }
+
+    if (!ARGUMENT_PRESENT(*BufferPointer)) {
+        return;
+    }
+
+    TraceStoreAllocatorAlignedFree(Context, *BufferPointer);
+    *BufferPointer = NULL;
+
+    return;
+}
+
+
+
+//
+// Initializer.
+//
+
 _Use_decl_annotations_
 BOOLEAN
 TraceStoreInitializeAllocator(
@@ -285,12 +758,31 @@ TraceStoreInitializeAllocator(
         TraceStoreAllocatorFreePointer,
         TraceStoreInitializeAllocator,
         TraceStoreDestroyAllocator,
+        TraceStoreAllocatorWriteBytes,
         TraceStoreAllocatorTryMalloc,
         TraceStoreAllocatorTryCalloc,
         TraceStoreAllocatorMallocWithTimestamp,
         TraceStoreAllocatorCallocWithTimestamp,
         TraceStoreAllocatorTryMallocWithTimestamp,
         TraceStoreAllocatorTryCallocWithTimestamp,
+        TraceStoreAllocatorAlignedMalloc,
+        TraceStoreAllocatorTryAlignedMalloc,
+        TraceStoreAllocatorAlignedMallocWithTimestamp,
+        TraceStoreAllocatorTryAlignedMallocWithTimestamp,
+        TraceStoreAllocatorAlignedOffsetMalloc,
+        TraceStoreAllocatorTryAlignedOffsetMalloc,
+        TraceStoreAllocatorAlignedOffsetMallocWithTimestamp,
+        TraceStoreAllocatorTryAlignedOffsetMallocWithTimestamp,
+        TraceStoreAllocatorAlignedCalloc,
+        TraceStoreAllocatorTryAlignedCalloc,
+        TraceStoreAllocatorAlignedCallocWithTimestamp,
+        TraceStoreAllocatorTryAlignedCallocWithTimestamp,
+        TraceStoreAllocatorAlignedOffsetCalloc,
+        TraceStoreAllocatorTryAlignedOffsetCalloc,
+        TraceStoreAllocatorAlignedOffsetCallocWithTimestamp,
+        TraceStoreAllocatorTryAlignedOffsetCallocWithTimestamp,
+        TraceStoreAllocatorAlignedFree,
+        TraceStoreAllocatorAlignedFreePointer,
         NULL
     );
 
