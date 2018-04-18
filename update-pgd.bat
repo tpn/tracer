19361e3ef7202f02159e395b330b6ea3a642d4b2
Trent Nelson <trent@trent.me>
2018-04-17 20:58:19 -0400

    Add a helper script for merging PGO .pgc files back into their corresponding .pgd file.

---
+++ update-pgd.bat
@@ -0,0 +1,16 @@
+@echo off
+
+setlocal enableextensions
+
+for /f "usebackq tokens=1,2,3 delims=!." %%i in (`dir /b x64\PGInstrument\*.pgc`) do (
+
+    rem If input is Rtl!1.pgc, then:
+    rem 	i = Rtl
+    rem		j = 1
+    rem		k = pgc
+    rem
+    rem	We want to generate:
+    rem		pgomgr /merge x64\PGInstrument\Rtl!1.pgc x64\Rtl.pgd
+
+    @pgomgr /merge "x64\PGInstrument\%%i!%%j.%%k" x64\%%i.pgd
+)
