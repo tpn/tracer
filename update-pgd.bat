bad02e574d74c84f8051d6c63da27e055fd115b3
Trent Nelson <trent@trent.me>
2018-04-22 19:55:25 -0400

    Tweak update-pgd.bat.

--- update-pgd.bat
+++ update-pgd.bat
@@ -2,15 +2,16 @@
 
 setlocal enableextensions
 
-for /f "usebackq tokens=1,2,3 delims=!." %%i in (`dir /b x64\PGInstrument\*.pgc`) do (
+rem If input is Rtl!1.pgc, then:
+rem 	i = Rtl
+rem		j = 1
+rem		k = pgc
+rem
+rem	We want to generate:
+rem		pgomgr /merge x64\PGInstrument\Rtl!1.pgc x64\Rtl.pgd
 
-    rem If input is Rtl!1.pgc, then:
-    rem 	i = Rtl
-    rem		j = 1
-    rem		k = pgc
-    rem
-    rem	We want to generate:
-    rem		pgomgr /merge x64\PGInstrument\Rtl!1.pgc x64\Rtl.pgd
+echo on
 
-    @pgomgr /merge "x64\PGInstrument\%%i!%%j.%%k" x64\%%i.pgd
+@for /f "usebackq tokens=1,2,3 delims=!." %%i in (`dir /b x64\PGInstrument\*.pgc`) do (
+    pgomgr /merge "x64\PGInstrument\%%i!%%j.%%k" x64\%%i.pgd
 )
